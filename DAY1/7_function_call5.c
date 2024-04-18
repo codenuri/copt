26c88a84ee757def2bef1534c9bfafe584d6c933
ecourse <smkang@codenuri.co.kr>
2024-04-18 15:46:04 +0900

    Add files via upload

--- DAY1/7_function_call5.c
+++ DAY1/7_function_call5.c
@@ -7,8 +7,13 @@ __declspec(naked) void f2()
 {
 	__asm
 	{
-
-
+		// 인자에 접근 하는 방법.
+		// => ESP 레지스터를 사용해서 접근합니다.
+		// => 아래 코드는 "return 1번째인자 + 2번째 인자"
+		// => eax 에 담으면 반환 값의 의미 이미로
+		mov eax, dword ptr [esp-4]   // [] 가 *(esp-4)의 의미
+		add eax, dword ptr [esp-8]
+ 
 		ret   
 	}
 }
@@ -16,11 +21,13 @@ __declspec(naked) void f2()
 __declspec(naked) int f1()
 {
 	__asm
-	{
-		call f2  
+	{		
+		push 2
+		push 1 
+		call f2  // f2(1, 2) 의 의미 
 
 		//===================================== 이 윗부분만 이해해 보세요.
-		mov   eax, 10
+//		mov   eax, 10
 		ret
 	}
 }
