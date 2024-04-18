3e69de99e1347f6f5d5f08e92a31b987773d9b01
ecourse <smkang@codenuri.co.kr>
2024-04-18 14:25:35 +0900

    Add files via upload

--- DAY1/6_memory_pooling2.c
+++ DAY1/6_memory_pooling2.c
@@ -34,12 +34,22 @@ void destroy_pool()
 // 이제 총알이 필요하면 pool 에 요청하면 됩니다.
 bullet* pool_alloc()
 {
+	bullet* tmp = pool_head;
+	pool_head = pool_head->next;
 
+	return tmp;
 }
+
+// 아래 코드는
+// 가장 최근 반납한 총알이 pool 의 처음에 놓이게 됩니다.(pool_head)
+// => 이 경우, 다음에 요청시 "가장 최근에 사용한 메모리를 다시 할당" 하게 됩니다.
+// => 가장 최근에 사용한 메모리는 "캐쉬등에 남아 있을확율이 높습니다."
+// => 즉, 가장 빠르게 동작할수 있습니다.
 
 void pool_free(bullet* p)
 {
-
+	p->next = pool_head;
+	pool_head = p;
 }
 
 
