09ab5b11c3fb5eb4adbcb20846033b9c44645573
ecourse <smkang@codenuri.co.kr>
2024-04-18 14:43:32 +0900

    Add files via upload

--- DAY1/6_memory_pooling4.c
+++ DAY1/6_memory_pooling4.c
@@ -19,6 +19,10 @@ typedef struct _bullet
 // 아래 코드는 64bit 용
 typedef struct _bullet
 {
+	// 아래 공용체는
+	// 1. x, y 로 사용하거나(이름 없는 구조체가 핵심, 이름이 없어야 .x 로 바로 접근)
+	//												이름이 있으면 p->이름.x 로 해야 합니다.
+	// 2. * next 로 사용하거나!
 	union
 	{
 		struct
