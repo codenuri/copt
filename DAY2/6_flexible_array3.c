de2580bd8e050cc23e28c58f9ee647c168f75ff7
ecourse <smkang@codenuri.co.kr>
2024-04-19 11:11:16 +0900

    Add files via upload

--- DAY2/6_flexible_array3.c
+++ DAY2/6_flexible_array3.c
@@ -22,19 +22,20 @@ typedef struct _shapes
 int main()
 {
 //	int x[0]; // error
-	SHAPES s;
-	printf("%d\n", sizeof(s)); // 4
+//	SHAPES s;
+//	printf("%d\n", sizeof(s)); // 4
 
 	// 핵심 : flexible array 기술로 만든 구조체는 반드시 동적 메모리 할당으로 사용해야 합니다
 	SHAPES* s = (SHAPES*)malloc(sizeof(SHAPES) + sizeof(POINT) * 5);
 
 	// 위 코드의 결과는 count 와 5개의 배열이 선형적으로 (하나의 메모리)로 할당.
-	s.count = 5;
-	s.pos[0].x = 10;
-	s.pos[0].y = 10;
+	// => 그리고, POINT 배열 부분에 접근할때 pos 이름을 사용하면 됩니다.
+	s->count = 5;
+	s->pos[0].x = 10;
+	s->pos[0].y = 10;
 
-	s.pos[1].x = 20;
-	s.pos[1].y = 20;
+	s->pos[1].x = 20;
+	s->pos[1].y = 20;
 
 
 	free(s);
