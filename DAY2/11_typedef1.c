4728d561efb4ec6b981330b6639d1930552331e3
ecourse <smkang@codenuri.co.kr>
2024-04-19 13:30:12 +0900

    Add files via upload

---
+++ DAY2/11_typedef1.c
@@ -0,0 +1,11 @@
+// typedef  A  B : B 는 A의 별명(또다른 이름)
+// => 이렇게 해석 하면 이해 하기 어려운 코드가 있습니다 
+// => 아래 ARR 
+typedef int DWORD;
+typedef int ARR[2];
+
+int main()
+{
+	DWORD n; // int n 
+	ARR   a;
+}
