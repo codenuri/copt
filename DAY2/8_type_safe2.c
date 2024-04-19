41435ca34e2ad33c19c7c1ed5e551a1147b94414
ecourse <smkang@codenuri.co.kr>
2024-04-19 13:41:08 +0900

    Add files via upload

--- DAY2/8_type_safe2.c
+++ DAY2/8_type_safe2.c
@@ -16,5 +16,7 @@ int main()
 
 	// int 대신, HICON, HBITMAP 을 사용하므로 "코드 가독성"은 좋아 졌습니다.
 	// 그런데, 아래 코드가 컴파일 에러일까요 ?
-	DrawBitmap(icon_id); 
+	DrawBitmap(icon_id);  // HICON, HBITMAP 은 다른 타입이 아니라 같은 타입입니다.
+						  // 그래서 에러는 아닙니다.
+						  // 역시 "타입 안전성 부족!!"
 }
