8c63fe536a8eea5bcb2837c58cd6bc073500d9c6
ecourse <smkang@codenuri.co.kr>
2024-04-19 09:32:37 +0900

    Add files via upload

--- DAY2/9_macro2.c
+++ DAY2/9_macro2.c
@@ -3,7 +3,11 @@
 // 멀티 라인 매크로 기술
 //#define PRESS()	 printf("press enter"); getchar();  
 
-#define PRESS()	 { printf("press enter"); getchar(); }  
+// #define PRESS()	 { printf("press enter"); getchar(); }  
+
+// 아래 기술이 멀티 라인 매크로 함수를 만들때 가장 안전하게 만드는 기술 입니다.
+// => do ~ while(0) 매크로 기술
+#define PRESS()	 do { printf("press enter"); getchar(); } while(0)
 
 int main()
 {
@@ -19,7 +23,7 @@ int main()
 
 	// 아래 코드의 문제점과 해결책은
 	if (n == 1)
-		PRESS();
+		PRESS(); // { printf("press enter"); getchar(); };
 	else
 	{
 	}
