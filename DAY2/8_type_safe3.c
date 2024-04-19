b1d4b535f519894412a226c0e99e83c464f900e7
ecourse <smkang@codenuri.co.kr>
2024-04-19 13:50:26 +0900

    Add files via upload

--- DAY2/8_type_safe3.c
+++ DAY2/8_type_safe3.c
@@ -1,8 +1,10 @@
 
 // 아래 구조체는 구조체 자체를 사용하려고 만든 것이 아닙니다.
 // => 구조체의 포인터 타입만 사용하려고 만든것 입니다.
-struct ICON   {};
-struct BITMAP {};
+// C++ : 멤버가 없는 구조체를 만들수 있다.(empty class 라고 합니다.)
+// C   : 구조체는 반드시 멤버가 한개 이상 있어야 한다.
+struct ICON   { int nonused; };
+struct BITMAP { int nonused; };
 
 
 typedef struct ICON   *HICON;   // HICON   은 ICON*   타입
@@ -28,3 +30,6 @@ int main()
 //	DrawBitmap(icon_id); // error.
 						 // 타입 안전성이 좋아 졌습니다.
 }
+
+// 리눅스에서 "pthread_t" 타입이 위처럼 설계되었습니다.
+// windows api 에서 HICON, HBITMAP 등이 모두 위처럼 설계 되었습니다.
